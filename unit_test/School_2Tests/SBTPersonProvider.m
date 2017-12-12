46d05e7da9d90b630cb4587171ceb323794a80a1
admin <admin@wsmsk6n023.emea.media.global.loc>
2017-12-12 21:36:59 +0300

    first version of hw

---
+++ unit_test/School_2Tests/SBTPersonProvider.m
@@ -0,0 +1,72 @@
+//
+//  SBTPersonProvider.m
+//  School_2Tests
+//
+//  Created by admin on 12.12.17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import <Expecta/Expecta.h>
+#import "SBTPerson.h"
+#import "SBTPersonProvider.h"
+
+@interface MySBTPerson : NSObject
+
+@property (nonatomic, copy) NSString *firstName;
+@property (nonatomic, copy) NSString *secondName;
+@property (nonatomic, copy) NSString *lastName;
+
+@end
+
+@interface SBTPersonProviderTests : XCTestCase
+
+@property (nonatomic, strong) SBTPersonProvider *provider;
+
+@end
+
+@implementation SBTPersonProviderTests
+
+- (void)setUp {
+    [super setUp];
+    self.provider = OCMPartialMock([SBTPersonProvider new]);
+}
+
+- (void)tearDown {
+    self.provider = nil;
+    [super tearDown];
+}
+
+- (void)testGetPersonListFromJSONNilData
+{
+    NSArray *personList = [self.provider getPersonListFromJSON:nil];
+    expect(personList).to.beNil();
+}
+
+- (void)testGetPersonListFromJSONEmptyData
+{
+    NSArray *personList = [self.provider getPersonListFromJSON:@[]];
+    expect(personList).toNot.beNil();
+    expect(personList.count > 0).to.equal(NO);
+}
+
+- (void)testGetPersonListFromJSON
+{
+    id person = OCMClassMock([SBTPerson class]);
+    MySBTPerson *myPerson = [MySBTPerson new];
+    OCMStub([person new]).andReturn(myPerson);
+    
+    NSDictionary *dict = @{
+                            @"firstName" : @"Artem",
+                            @"lastName" : @"Lomov",
+                            @"secondName" : @"Sergeevich"
+                            };
+    NSArray *arr = @[dict];
+    NSArray *personList = [self.provider getPersonListFromJSON:arr];
+    
+    expect(personList[0].firstName).to.equal(@"Artem");
+}
+
+
+@end
